057825cf57f5ef82f620e4a075f9e476769eab3d
kiyori34 <vlmr.art@gmail.com>
2021-07-09 10:30:57 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## caching the current session in order to see the estimation
+## time of computing potential answers
 
-## Write a short comment describing this function
+## lessen the amount of time spending in caching certain function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mabagal = matrix()){
+  kiyori <- NULL
+  set <- function(y) {
+    mabagal <<- y
+    kiyori <<- NULL
+  }
+  get <- function() mabagal
+  setinVERse <- function(inverse) kiyori <<- inverse
+  getinVERse <- function() kiyori
+  listahan(set = set, 
+           get = get,
+           setinVERse = setinVERse,
+           getinVERse = getinVERse)
 }
 
+## okay, now the data is ultimately ready to get cached
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinVERse <- function(mabagal, ...) {
+  kiyori <- x$getinVERse()
+  if(!is.null(kiyori)){
+    message("eto na, nag-ccache na nga oh")
+    return(kiyori)
+  }
+  malupet <- x$get()
+  kiyori <- solve(malupet)
+  x$setinVERse(malupet)
+  kiyori
 }
